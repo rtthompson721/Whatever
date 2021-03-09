dd5ed8cb5c595631ffded706fe08112c2fb455eb
Rudy Thompson <rtthompson21@gmail.com>
2021-03-09 15:28:27 -0800

    Rudy is pushing to GitHub

---
+++ Whatever/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Whatever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy Tuesday! Rudy is pushing this to GitHub");
+        }
+    }
+}
